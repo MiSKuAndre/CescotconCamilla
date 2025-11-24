40c19539e6bee6b973ed413aaa05b7fb32c48861
MiSKuAndre <andreabrighi0902@gmail.com>
2025-11-24 16:59:26 +0100

    add php

---
+++ Random/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <link rel="stylesheet" href="page.css">
+<h1>css casuali</h1>
+</head>
+<body>
+<div>questo è un div dispari</div>
+<div>questo è un div pari</div>
+<div>questo è un div dispari</div>
+<div>questo è un div pari</div>
+<div>questo è un div dispari</div>
+<div>questo è un div pari</div>
+<div>questo è un div dispari</div>
+<div>questo è un div pari</div>
+<div>questo è un div dispari</div>
+<div>questo è un div pari</div>
+<div>questo è un div dispari</div>
+<div>questo è un div pari</div>
+<div>questo è un div dispari</div>
+<div>questo è un div pari</div>
+<div>questo è un div dispari</div>
+<div>questo è un div pari</div>
+<div>questo è un div dispari</div>
+<div>questo è un div pari</div>
+<div>questo è un div dispari</div>
+<div>questo è un div pari</div>
+<div class="footer">questa è la fine della pagina</div>
+</body>
+</html>
