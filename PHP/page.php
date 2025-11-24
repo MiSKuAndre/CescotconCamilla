1ee109f2e71234dd9429c2f20131393072c32a88
MiSKuAndre <andreabrighi0902@gmail.com>
2025-11-24 16:07:44 +0100

    add php e html

---
+++ PHP/page.php
@@ -0,0 +1,22 @@
+<head>
+    <link rel="stylesheet" href="page.css">
+<h1>pagina di prova</h1>
+</head>
+<body>
+<div>questo è il div numero 1</div>
+<div>questo è il div numero 2</div>
+<div>questo è il div numero 3</div>
+<div>questo è il div numero 4</div>
+<div>questo è il div numero 5</div>
+<div>questo è il div numero 6</div>
+<div>questo è il div numero 7</div>
+<div>questo è il div numero 8</div>
+<div>questo è il div numero 9</div>
+<div>questo è il div numero 10</div>
+<div>questo è il div numero 11</div>
+<div>questo è il div numero 12</div>
+<div>questo è il div numero 13</div>
+<div>questo è il div numero 14</div>
+<div>questo è il div numero 15</div>
+<div class="footer">questo è la fine della pagina</div>
+</body>
