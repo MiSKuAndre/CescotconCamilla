2cd23d953334c6792615152531efb5c58981e140
MiSKuAndre <andreabrighi0902@gmail.com>
2025-11-24 17:04:46 +0100

    php sistemato

--- Random/index.php
+++ Random/index.php
@@ -6,26 +6,26 @@
 <h1>css casuali</h1>
 </head>
 <body>
-<div>questo è un div dispari</div>
-<div>questo è un div pari</div>
-<div>questo è un div dispari</div>
-<div>questo è un div pari</div>
-<div>questo è un div dispari</div>
-<div>questo è un div pari</div>
-<div>questo è un div dispari</div>
-<div>questo è un div pari</div>
-<div>questo è un div dispari</div>
-<div>questo è un div pari</div>
-<div>questo è un div dispari</div>
-<div>questo è un div pari</div>
-<div>questo è un div dispari</div>
-<div>questo è un div pari</div>
-<div>questo è un div dispari</div>
-<div>questo è un div pari</div>
-<div>questo è un div dispari</div>
-<div>questo è un div pari</div>
-<div>questo è un div dispari</div>
-<div>questo è un div pari</div>
+<div class="dispari">questo è un div dispari</div>
+<div class="pari">questo è un div pari</div>
+<div class="dispari">questo è un div dispari</div>
+<div class="pari">questo è un div pari</div>
+<div class="dispari">questo è un div dispari</div>
+<div class="pari">questo è un div pari</div>
+<div class="dispari">questo è un div dispari</div>
+<div class="pari">questo è un div pari</div>
+<div class="dispari">questo è un div dispari</div>
+<div class="pari">questo è un div pari</div>
+<div class="dispari">questo è un div dispari</div>
+<div class="pari">questo è un div pari</div>
+<div class="dispari">questo è un div dispari</div>
+<div class="pari">questo è un div pari</div>
+<div class="dispari">questo è un div dispari</div>
+<div class="pari">questo è un div pari</div>
+<div class="dispari">questo è un div dispari</div>
+<div class="pari">questo è un div pari</div>
+<div class="dispari">questo è un div dispari</div>
+<div class="pari">questo è un div pari</div>
 <div class="footer">questa è la fine della pagina</div>
 </body>
 </html>
