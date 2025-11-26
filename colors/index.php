f028131af91956c1898d88a7234357b296826bf3
andrea <andreabrighi0902@gmail.com>
2025-11-26 16:45:11 +0100

    php finito

--- colors/index.php
+++ colors/index.php
@@ -6,8 +6,14 @@
 
     </head>
     <body>
+<?php
+    $numero = rand(5, 30);
 
+    for($i = 1; $i <= $numero; $i++) {
+        echo "<div class='first'>div casuali</div>";
+    }
+?>
 
-
+    <div class="footer">fine pagina</div>
     </body>
 </html>
