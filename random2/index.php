fe813448f0c6f8b1e8df9fe2981ca3d826be77cc
Cams82 <camsprom@gmail.com>
2025-11-26 16:08:55 +0100

    php

--- random2/index.php
+++ random2/index.php
@@ -1,14 +1,23 @@
 <html>
     <head>
+        <link href="style.css">
+
     <h1>Contenuti a caso!</h1>
 
     </head>
 
 
 <?php
+    // Numero casuale tra 10 e 30
 
+    $numero = rand(5, 15);
 
+    // Ciclo per creare i div da 1 a $numero
 
+    for($i = 1; $i <= $numero; $i++){
+        echo "<div>Io sono dispari</div>";
+        echo "<div>Io sono pari</div>";
+    }
+    
 ?>
 
-</html>
