6b31caa5787d242b050d5c6ccf1b133d4504327a
Cams82 <camsprom@gmail.com>
2025-11-26 17:31:39 +0100

    boh

--- colors/index.php
+++ colors/index.php
@@ -12,6 +12,28 @@
     for($i = 1; $i <= $numero; $i++) {
         echo "<div class='first'>div casuali</div>";
     }
+
+    $colori = ["red", "green", "blue", "yellow"];
+
+    function coloreCasuale($colori){
+        return $colori[array_rand($colori)];
+    }
+
+    for (&i = 1; $i <= $numero; $i++){
+        $testoColore = coloreCasuale($colori);
+
+        switch ($testoColore){
+            case "red":
+                $bg = "red";
+                break;
+            case "green":
+                $bg = "green";
+            case "blue":
+                $bg = "blue";
+            case "yellow":
+                $bg = "yellow";
+        }
+    }
 ?>
 
     <div class="footer">fine pagina</div>
