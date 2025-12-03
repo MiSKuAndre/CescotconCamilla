f423dcf7d842fb24e67dba24c7ca0833e0e80a0a
Cams82 <camsprom@gmail.com>
2025-12-03 16:10:35 +0100

    add initial HTML structure and PHP function for events page

---
+++ Events/index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Events Page</title>
+    <link rel="stylesheet" href="">   
+    <script src="eventi.js"></script>
+</head>
+<body>  
+    <h1>Eventi JavaScript</h1>
+    <button id="Colora">Colora</button>
+
+</body>
+</html>
+
+<?php
+
+function printDiv($text, $class) {
+    echo "<div class="" . $class . "">" . $text . "</div>";
+};
+
+?>
