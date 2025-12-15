37beecc305cb2c7a1e444a5920cd91edb609e5d2
Cams82 <camsprom@gmail.com>
2025-12-15 11:53:57 +0100

    aggiunta struttura iniziale per il file index.php

---
+++ Groups/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Groups Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <button id="Primorosso">Primo rosso</button>
+    <button id="Secondorosso">Secondo rosso</button>
+    <button id="Tuttirosso">Tutti rosso</button>
+    <button id="Primoblu">Primo blu</button>
+    <button id="Secondoblu">Secondo blu</button>
+    <button id="Tuttiblu">Tutti blu</button>
+
+
+
+<h2 class="first">Primo gruppo</h2>
+
+<h2 class="second">Secondo gruppo</h2>
+
+
+
+
+
+<script src="groups.js"></script>
+
+</body>
