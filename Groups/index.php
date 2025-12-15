a2b7e234e7f931e0d7339e0d36bafcbb63d4e411
Cams82 <camsprom@gmail.com>
2025-12-15 12:44:05 +0100

    Refactor div structure and update random div generation logic for both groups

--- Groups/index.php
+++ Groups/index.php
@@ -19,26 +19,27 @@
     <button id="Primoblu">Primo blu</button>
     <button id="Secondoblu">Secondo blu</button>
     <button id="Tuttiblu">Tutti blu</button>
-
-
-
 <h2 class="first">Primo gruppo</h2>
+<div class="display-inline">
 
 <?php
     // Genera un numero di div casuale tra 5 e 10 di dimensione 150x150 px
     RandomDivDa5a10();
 
 ?>
+</div>
+
+
 
 <h2 class="second">Secondo gruppo</h2>
-
+<div class="display-inline">
 <?php
-    // Genera un numero di div casuale tra 5 e 10 di dimensione 100x100 px
+    // Genera un numero di div casuale tra 10 e 15 di dimensione 100x100 px
     RandomDivDa10a15();
 
 ?>
 
-
+</div>
 
 <script src="gruppi.js"></script>
 
