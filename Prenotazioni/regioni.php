f4e651b2b866b77bf92b9df71e0cad6c3616e50c
Cams82 <camsprom@gmail.com>
2026-01-15 12:56:10 +0100

    regioni

---
+++ Prenotazioni/regioni.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Regioni</h1>
+    <div>
+    <?php
+
+    // Connessione al database
+    $conn = new mysqli("localhost", "root", "", "prenotazioni");
+    // Controllo della connessione
+    if ($conn->connect_error) {
+        die("Connessione fallita: " . $conn->connect_error);
+    }      
+    // Query per ottenere i dati delle prenotazioni con join tra le tabelle citta, clienti e prenotazioni   
+    $sql = "SELECT regioni.regione, sum(prenotazioni.caparra) as caparra, count(*) as numero_prenotazioni, round(sum(prenotazioni.importo), 2) as importo, round(sum(prenotazioni.importo - prenotazioni.caparra), 2) as saldo
+            FROM regioni
+            INNER JOIN citta ON regioni.id_regione = citta.regione
+            INNER JOIN clienti ON citta.id_citta = clienti.citta
+            INNER JOIN prenotazioni ON clienti.id_cliente = prenotazioni.cliente
+            group by regioni.regione";
+
+    $result = $conn->query($sql);
+
+    // Controllo se ci sono risultati e stampa del testo
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "<div class='prenotazioni'>";
+            echo "<h2>" . $row["regione"] . "</h2>";
+            echo "<p>Numero prenotazioni: " . $row["numero_prenotazioni"] . "</p>";
+            echo "<p>Importo totale: " . $row["importo"] . "</p>";
+            echo "<p class='saldo'>Saldo: " . $row["saldo"] . "</p><br>";
+            echo "</div>";
+        }
+    } else {
+        echo "Nessun dato trovato.";
+    }
+
+    // Chiusura della connessione
+    $conn->close();
+    ?>
+    </div>
